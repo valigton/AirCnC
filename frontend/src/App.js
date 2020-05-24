b40b2b8833ee362083e22d6d345900406be58d3f
valigton <valigtonverissimo@outlook.com>
2020-05-24 19:01:08 -0300

    Logon finalizado

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import api from './services/api'
+import './App.css';
+
+import logo from './assets/logo.svg'
+
+function App() {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await api.post('/sessions', { email });
+
+    const { _id } = response.data;
+
+    console.log(_id);
+  }
+
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCnC"/>
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre 
+           <strong> talentos</strong> para sua empresa.
+        </p>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+            type="email" 
+            id="email" 
+            placeholder="Seu melhor email" 
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+export default App;
