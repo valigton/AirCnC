07f562f8c6618a179a5f9e36936a596d0b48f8ba
valigton <valigtonverissimo@outlook.com>
2020-05-24 21:23:50 -0300

    frontend finalizado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,17 @@
-import React, { useState } from 'react';
-import api from './services/api'
+import React from 'react';
+import Routes from './routes';
+
 import './App.css';
+import logo from './assets/logo.svg';
 
-import logo from './assets/logo.svg'
-
-function App() {
-  const [email, setEmail] = useState('');
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    const response = await api.post('/sessions', { email });
-
-    const { _id } = response.data;
-
-    console.log(_id);
-  }
-
+export default function App() {
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
+      
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre 
-           <strong> talentos</strong> para sua empresa.
-        </p>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input 
-            type="email" 
-            id="email" 
-            placeholder="Seu melhor email" 
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
 }
-export default App;
